acadcb0afbd546e460c02bced2dfe1c4274557d0
untermensch <anon@anon.net>
2012-10-21 15:43:26 -0700

    add search all partitions for data2sd label

--- init_rd/sbin/data2sd.sh
+++ init_rd/sbin/data2sd.sh
@@ -1,11 +1,16 @@
 #!/system/bin/sh
 
 DEVPATH=/dev/block/
-DATA2SD=mmcblk1p1
+DATA2SD=mmcblk1
 DATA=mmcblk0p8
 BUSYBOX=/system/xbin/busybox
 MOUNTOPT=noauto_da_alloc,noatime,nosuid,nodev,nomblk_io_submit,errors=panic
 FSTYPE=ext4
+
+# check for external sd card
+if [ ! -e $DEVPATH$DATA2SD ] ; then
+	exit 1
+fi
 
 # wait for system to mount
 while [ ! -d /system/app ] ; do
@@ -17,7 +22,8 @@ if [ ! -e $BUSYBOX ] ; then
 fi
 
 # check superblock for "data2sd" remount data if needed
-if [ -b $DEVPATH$DATA2SD ] ; then
+for PARTITION in $DEVPATH$DATA2SD* ; do
+	DATA2SD=${PARTITION#$DEVPATH}
 	if $BUSYBOX dd if=$DEVPATH$DATA2SD skip=1144 bs=1 count=16 | $BUSYBOX grep data2sd ; then
 		$BUSYBOX mount -o rw,remount -t rootfs rootfs /
 		$BUSYBOX mkdir /internal_sd
@@ -28,6 +34,6 @@ if [ -b $DEVPATH$DATA2SD ] ; then
 		$BUSYBOX mkdir /data/media/internal_sd
 		$BUSYBOX mount -o bind /internal_sd/media /data/media/internal_sd
 	fi
-fi
+done
 
 exit 0
